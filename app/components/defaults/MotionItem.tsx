d929bee6687f14495beeb411cf2cc1bd248f162e
ahmad655656 <ahmadsalloum8899@gmail.com>
2025-06-13 10:57:45 +0300

    end

--- app/components/defaults/MotionItem.tsx
+++ app/components/defaults/MotionItem.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import { motion } from 'framer-motion'
-const MotionItem = ({children, className, initial, animate} : {children: React.ReactNode, className: string, initial?: any, animate?: any}) => {
+const MotionItem = ({children, className, initial, animate} : {children: React.ReactNode, className?: string, initial?: any, animate?: any}) => {
   return (
     <motion.div initial={initial} whileInView={animate} className={`${className}`}>
         {children}
